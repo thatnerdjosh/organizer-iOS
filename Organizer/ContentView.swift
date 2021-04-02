8515106059c1a9f90c69562704d6318dc1362438
Josh Santos <josh@omnidapps.com>
2021-04-03 06:46:18 +0700

    Add scaffold project

---
+++ Organizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Organizer
+//
+//  Created by Josh Santos on 3/4/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Organizer/OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizerApp.swift
+//  Organizer
+//
+//  Created by Josh Santos on 3/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
